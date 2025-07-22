3b7b80d160ca43d4ef8227f867a699f24e9a7a4b
fr-informatique <fabian.measson123@gmail.com>
2025-07-22 20:46:50 +0200

    Fix missing isAnswerCorrectWithTolerance import

--- contexts/QuizContext.tsx
+++ contexts/QuizContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { isAnswerCorrectWithTolerance } from '../utils/helpers';
 
 export interface QuizItem {
   id: string;
